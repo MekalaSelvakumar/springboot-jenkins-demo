be2d7d27ed183d88d0f10a73913e810d5c2ba00e
r.selvakumar <r.selvakumar@stackroute.in>
2023-04-06 16:40:22 +0530

    first

---
+++ src/main/java/com/example/springbootjenkindemo/SpringbootJenkinDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootjenkindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJenkinDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJenkinDemoApplication.class, args);
+	}
+
+}
